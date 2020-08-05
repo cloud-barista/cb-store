f486d9fa41fe24476228f9484b67a3d9b6dea756
ccambomorris <ccambomorris@gmail.com>
2020-08-05 16:58:29 +0900

    08-05 : ETCD Endpoints 다중서버 적용

--- go.mod
+++ go.mod
@@ -13,18 +13,28 @@ require (
 	github.com/coreos/go-semver v0.3.0 // indirect
 	github.com/coreos/go-systemd v0.0.0-00010101000000-000000000000 // indirect
 	github.com/coreos/pkg v0.0.0-20180928190104-399ea9e2e55f // indirect
-	github.com/envoyproxy/go-control-plane v0.9.4 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/gogo/protobuf v1.3.1 // indirect
-	github.com/golang/protobuf v1.3.3 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/google/btree v1.0.0 // indirect
 	github.com/google/uuid v1.1.1 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/grpc-ecosystem/go-grpc-middleware v1.2.0 // indirect
+	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway v1.14.6 // indirect
+	github.com/jonboulle/clockwork v0.2.0 // indirect
+	github.com/prometheus/client_golang v1.7.1 // indirect
 	github.com/sirupsen/logrus v1.6.0
 	github.com/snowzach/rotatefilehook v0.0.0-20180327172521-2f64f265f58c // indirect
+	github.com/soheilhy/cmux v0.1.4 // indirect
+	github.com/tmc/grpc-websocket-proxy v0.0.0-20200427203606-3cfed13b9966 // indirect
+	github.com/xiang90/probing v0.0.0-20190116061207-43a291ad63a2 // indirect
 	github.com/xujiajun/nutsdb v0.5.1-0.20200320023740-0cc84000d103
 	go.etcd.io/bbolt v1.3.4 // indirect
 	go.etcd.io/etcd v3.3.18+incompatible
 	go.uber.org/zap v1.15.0 // indirect
-	golang.org/x/sys v0.0.0-20200602225109-6fdc65e7d980 // indirect
-	google.golang.org/grpc v1.26.0 // indirect
+	golang.org/x/time v0.0.0-20200630173020-3af7569d3a1e // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200603094226-e3079894b1e8
+	sigs.k8s.io/yaml v1.2.0 // indirect
 )
--- store-drivers/etcd-driver/etcd-driver.go
+++ store-drivers/etcd-driver/etcd-driver.go
@@ -8,10 +8,12 @@
 package etcddriver
 
 import (
-	"go.etcd.io/etcd/clientv3"
 	"context"
-	"time"
 	"strconv"
+	"strings"
+	"time"
+
+	"go.etcd.io/etcd/clientv3"
 
 	"github.com/cloud-barista/cb-store/config"
 	icbs "github.com/cloud-barista/cb-store/interfaces"
@@ -23,58 +25,61 @@ var ctx context.Context
 type ETCDDriver struct{}
 
 func init() {
-	etcdServerPort := config.GetConfigInfos().ETCD.ETCDSERVERPORT  
-//	config.Cblogger.Info("serverPort:" + etcdServerPort)
+	etcdServerPort := config.GetConfigInfos().ETCD.ETCDSERVERPORT
+	//	config.Cblogger.Info("serverPort:" + etcdServerPort)
 
-        etcdcli, err := clientv3.New(clientv3.Config{
-                Endpoints:   []string{"http://" + etcdServerPort}, // @TODO set multiple Server
-                DialTimeout: 5 * time.Second,
-        })
-	
+	etcdcli, err := clientv3.New(clientv3.Config{
+		// Original
+		// Endpoints:   []string{"http://" + etcdServerPort}, // @TODO set multiple Server
+		// Modified by ccambomorris
+		Endpoints:   strings.Split(etcdServerPort, ","),
+		DialTimeout: 5 * time.Second,
+	})
+
 	//config.Cblogger.Infof("etcdcli: %#v",  etcdcli)
 
 	if err != nil {
-                config.Cblogger.Error(err)
-        }
+		config.Cblogger.Error(err)
+	}
 
-        cli = etcdcli
+	cli = etcdcli
 	ctx = context.Background()
 }
 
 func (etcdDriver *ETCDDriver) InitDB() error {
-        config.Cblogger.Info("init db")
+	config.Cblogger.Info("init db")
 
-        _, err := cli.Delete(ctx, "/", clientv3.WithPrefix()) // @todo
+	_, err := cli.Delete(ctx, "/", clientv3.WithPrefix()) // @todo
 
-        if err != nil {
-                config.Cblogger.Error(err)
-        }
+	if err != nil {
+		config.Cblogger.Error(err)
+	}
 
-        return err
-        return nil
+	return err
+	return nil
 }
 
 func (etcdDriver *ETCDDriver) InitData() error {
-        config.Cblogger.Info("init data")
+	config.Cblogger.Info("init data")
 
-        _, err := cli.Delete(ctx, "/", clientv3.WithPrefix())
+	_, err := cli.Delete(ctx, "/", clientv3.WithPrefix())
 
-        if err != nil {
-                config.Cblogger.Error(err)
-        }
+	if err != nil {
+		config.Cblogger.Error(err)
+	}
 
-        return err
+	return err
 	return nil
 }
 
 func (etcdDriver *ETCDDriver) Put(key string, value string) error {
-	config.Cblogger.Info("Key:" + key  + ", value:" + value)
+	config.Cblogger.Info("Key:" + key + ", value:" + value)
 
-        _, err := cli.Put(ctx, key, value)
+	_, err := cli.Put(ctx, key, value)
 	if err != nil {
-                config.Cblogger.Error(err)
+		config.Cblogger.Error(err)
 		return err
-        }
+	}
 
 	return nil
 }
@@ -82,23 +87,23 @@ func (etcdDriver *ETCDDriver) Put(key string, value string) error {
 func (etcdDriver *ETCDDriver) Get(key string) (*icbs.KeyValue, error) {
 	config.Cblogger.Info("Key:" + key)
 
-        resp, err := cli.Get(ctx, key)
-        if err != nil {
-                config.Cblogger.Error(err)
+	resp, err := cli.Get(ctx, key)
+	if err != nil {
+		config.Cblogger.Error(err)
 		return nil, err
-        }
+	}
 
-        for _, ev := range resp.Kvs {
+	for _, ev := range resp.Kvs {
 		keyValue := icbs.KeyValue{string(ev.Key), string(ev.Value)}
-                return &keyValue, nil
-        }
+		return &keyValue, nil
+	}
 
-        //return nil, fmt.Errorf("No Results with %s Key!!", key)
-        return nil, nil
+	//return nil, fmt.Errorf("No Results with %s Key!!", key)
+	return nil, nil
 }
 
 func (etcdDriver *ETCDDriver) GetList(key string, sortAscend bool) ([]*icbs.KeyValue, error) {
-        config.Cblogger.Info("Key:" + key + ", sortAscend:" + strconv.FormatBool(sortAscend))
+	config.Cblogger.Info("Key:" + key + ", sortAscend:" + strconv.FormatBool(sortAscend))
 
 	order := clientv3.SortAscend
 
@@ -107,34 +112,33 @@ func (etcdDriver *ETCDDriver) GetList(key string, sortAscend bool) ([]*icbs.KeyV
 	}
 	resp, err := cli.Get(ctx, key, clientv3.WithPrefix(), clientv3.WithSort(clientv3.SortByKey, order))
 
-        if err != nil {
-                config.Cblogger.Error(err)
-                return nil, err
-        }
+	if err != nil {
+		config.Cblogger.Error(err)
+		return nil, err
+	}
 
-        keyValueList := make([]*icbs.KeyValue, len(resp.Kvs))
-        for k, ev := range resp.Kvs {
-                tmpOne := icbs.KeyValue{string(ev.Key), string(ev.Value)}
-                keyValueList[k] = &tmpOne
-        }
+	keyValueList := make([]*icbs.KeyValue, len(resp.Kvs))
+	for k, ev := range resp.Kvs {
+		tmpOne := icbs.KeyValue{string(ev.Key), string(ev.Value)}
+		keyValueList[k] = &tmpOne
+	}
 
-        return keyValueList, nil
+	return keyValueList, nil
 }
 
-func (etcdDriver *ETCDDriver)Delete(key string) error {
+func (etcdDriver *ETCDDriver) Delete(key string) error {
 	config.Cblogger.Info("Key:" + key)
 
 	_, err := cli.Delete(ctx, key)
-	
-        if err != nil {
-                config.Cblogger.Error(err)
-        }
+
+	if err != nil {
+		config.Cblogger.Error(err)
+	}
 
 	return err
 }
 
 func Close() {
-        cli.Close()
+	cli.Close()
 	cli = nil
 }
-
